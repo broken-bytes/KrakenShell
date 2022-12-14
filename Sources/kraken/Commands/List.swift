f38a98d94a6cda92ea380170695a920c697912cc
Marcel Kulina <marcel.kulina@outlook.de>
2022-12-14 14:14:35 +0100

    Refactor some code smells

--- Sources/kraken/Commands/List.swift
+++ Sources/kraken/Commands/List.swift
@@ -15,22 +15,21 @@ class List: Command {
         let ptr = UnsafeMutablePointer<UInt8>.allocate(capacity: 1024)
         let pathPtr = getcwd(ptr, 1024)
         let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
 
         var items: [Any] = []
         do {
-            let items = try fm.contentsOfDirectory(atPath: path)
+            let items = try fm.contentsOfDirectory(atPath: pathPtr)
             var entries: [DataTableEntry] = []
 
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "MMM dd,yyyy"
             for item in items {
                 let attributes = try FileManager.default.attributesOfItem(atPath: path.appending("/\(item)"))
-                let fileSize = attributes[FileAttributeKey.size] as! UInt64
+                let fileSize = attributes[FileAttributeKey.size] as? UInt64
                 let modifyDate = attributes[FileAttributeKey.modificationDate] as? Date
 
                 let nameField = DataField(name: "name", value: item, type: .text)
-                let sizeField = DataField(name: "size", value: String(fileSize), type: .number)
+                let sizeField = DataField(name: "size", value: String(fileSize ?? 0), type: .number)
                 let modifyField = DataField(name: "size", value: (modifyDate ?? Date()), type: .number)
                 let entry = DataTableEntry(fields: [nameField, sizeField, modifyField])
 
